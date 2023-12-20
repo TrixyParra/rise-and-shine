6954e8af43991290c5ff482c5440dd09e013c70a
Trixy Parra <trixy.parra@gmail.com>
2023-12-20 00:09:37 -0800

    added mobile menu toggle

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const hamburgerMenu = document.querySelector('.nav-toggle');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 hamburgerMenu.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('is-active')
+    mobileMenu.classList.toggle('is-open')
 });
